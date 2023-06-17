e3890e935389273ce990ca420ee53eff0bed7d75
Daniyl <daniyl-x@proton.me>
2023-06-17 22:43:36 +0300

    first commit

---
+++ learning_logs/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Topic(models.Model):
+    """Тема, которую изучает пользователь"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, models.CASCADE)
+
+    def __str__(self):
+        """Возвращает строковое представление модели"""
+        return self.text
+
+
+class Entry(models.Model):
+    """Информация, изученная пользователем по теме"""
+    topic = models.ForeignKey(Topic, models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Возвращает строковое представление модели"""
+        text_preview = self.text[:50]
+        if len(self.text) >= 50:
+            text_preview += '...'
+        return text_preview
